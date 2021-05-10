1d694e9dbe7ac6de9bf2bd0151680c73dc698d3c
Saberzhan19 <Saberzhax@gmail.com>
2021-05-11 05:43:07 +0600

    Style

--- register.php
+++ register.php
@@ -9,13 +9,19 @@
     
     <!-- Форма регистраций -->
     <form method="post" action="">
+        <label>Fullname</label>
+        <input type="text" name="" placeholder="Write your fullname">
         <label>Login</label>
         <input type="text" name="" placeholder="Write your login">
+        <label>Email</label>
+        <input type="email" name="" placeholder="Write your email">
+        <label>Avatar</label>
+        <input type="file">
         <label>Password</label>
         <input type="password" name="" placeholder="Write your password">
         <button>Text</button>
         <p>
-            Do you have not account? <a href ="/register.php">Registration</a>
+            Do you have not account? <a href ="#">Registration</a>
         </p>
     </form>
 

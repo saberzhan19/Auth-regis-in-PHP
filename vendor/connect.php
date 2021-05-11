c4ec98f6faada290fb04876274cbb2cf0aada9d3
Saberzhan19 <Saberzhax@gmail.com>
2021-05-11 06:26:00 +0600

    PHP

---
+++ vendor/connect.php
@@ -0,0 +1,12 @@
+<?php 
+
+//соединяемся с базой данных
+
+$connect = mysqli_connect('localhost','root','root','logreg3');
+
+//проверяем соединение с БД
+if(!$connect){
+    die('Error connect to Database');
+} else{
+    echo 'Working!';
+}
